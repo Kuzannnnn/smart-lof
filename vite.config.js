b473c2e962c28f035f90207a35b3008e0512650e
Kuzannnnn <siglos.daleraven@gmail.com>
2026-03-01 14:58:01 +0800

    chore: update deployment config and add nojekyll

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Change this from 'smart-lof-web' to 'smart-lof'
+  base: '/smart-lof/', 
   resolve: {
-    extensions: ['.js', '.jsx', '.json'] // Helps resolve imports without .jsx
+    extensions: ['.js', '.jsx', '.json']
   }
 })
